934caeb6072582038c5d09489b19e6c7008d68a7
tanner.dailey <daileytanner@gmail.com>
2020-09-11 12:00:10 -0600

    finished javascript-2

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -6,25 +6,39 @@
 
 // Code Here
 
+const lovesCode = true;
+
 //////////////////STEP 2////////////////////
 //Create a variable called 'age' that has a value equal to your age as a number. 
 
 // Code Here
+
+const age = 24;
 
 //////////////////STEP 3////////////////////
 //Create a variable called 'name' and assign it a value of your name as a string.  
 
 // Code Here
 
+const name = 'Tanner';
+
 //////////////////STEP 4////////////////////
 //Create a variable called 'phrase' that is initialized as an empty string. 
 
 // Code Here
 
+let phrase = '';
+
 //////////////////STEP 5////////////////////
 //Create an if statement that checks to see if the lovesCode variable is equal to true. If it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
 // Code Here
+
+if(lovesCode === true){
+    phrase = 'I love code';
+} else {
+    phrase = 'I do not love to code';
+}
 
 //////////////////Wrap Up////////////////////
 // Refresh the index.html page in your browser. You should now see a poem that uses the variables from above.
